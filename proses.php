99f79a9a706086f2f6cbbf17145a6cf783bef969
Fahrizky Syihabudin I <101689299+Riez03@users.noreply.github.com>
2022-07-20 21:11:35 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -8,7 +8,8 @@
     $jumlah=$_POST['jumlah'];
     $pesan=$_POST['pesan'];
 
-    echo "<head><title>Data Pemesanan</head></head>";
+    header("Location: pesan.html");
+
     $fp = fopen("pesanan.txt","a+");
     fputs($fp,"$tgl|$nama|$alamat|$telp|$material|$merek|$jumlah|$pesan\n");
     fclose($fp);
@@ -17,6 +18,4 @@
     fputs($fp,"Struct Pemesanan di Toko Bangunan\n");
     fputs($fp,"Tanggal : $tgl\nNama : $nama\nAlamat : $alamat\nNo. Telp/ Wa : $telp\nMaterial : $material\nMerek : $merek\nJumlah : $jumlah\nPesan : $pesan\n\n");
     fclose($fp);
-
-    header("Location: pesan.html");
 ?>
